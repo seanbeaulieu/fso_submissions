661c722de0c69a7d168a9ec96e6fefc2b6dca483
Sean Beaulieu <54913639+seanbeaulieu@users.noreply.github.com>
2024-02-10 22:16:24 -0600

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 // search filter
 const SearchFilter = (props) => {
@@ -57,15 +58,24 @@ const Persons = (props) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterNames] = useState('')
+
+  // effectHook here
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:8001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const handleFilterChange = (event) => { 
 
