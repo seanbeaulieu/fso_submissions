2f5b46f9ea6de07e707304649688ac069e76ca16
Sean Beaulieu <54913639+seanbeaulieu@users.noreply.github.com>
2024-02-15 21:39:28 -0600

    part3 setup

---
+++ part3/helloworld/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+// ROOT 
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+// GET ALL RESOURCE
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+// GET SPECIFIC RESOURCE    
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+  })
+
+// DELETE
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    
+    response.status(204).end()
+  })
+
+
+// POST
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+  response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
